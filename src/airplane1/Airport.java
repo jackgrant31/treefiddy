dd6b7158b29d1bad692ec0a254cd8db09a3a1de5
jack <jack@wg-dhcp160d169.fsuguest.wireless.fsu.edu>
2018-03-05 15:42:48 -0500

    fixed static

--- src/airplane1/Airport.java
+++ src/airplane1/Airport.java
@@ -3,7 +3,7 @@ package airplane1;
 public class Airport {
 	
 	public static void main(String args[]) {
-		op800();
+		opA320();
 	}
 	
 	public static void op800() { //to populate 800 plane
@@ -12,13 +12,13 @@ public class Airport {
 				ei.populateArray();
 	}
 	
-	public void op700() { //to populate 700 plane
+	public static void op700() { //to populate 700 plane
 		//for700
 				Plane700 se = new Plane700(118);
 				se.populateArray();
 	}
 	
-	public void opA320() { //to populate A320 plane
+	public static void opA320() { //to populate A320 plane
 		//forA320
 				PlaneA320 a3 = new PlaneA320(150);
 				a3.populateArray();
